77a26bb23ecafd67b51372c20e04b599ab929b52
g.carignano <g.carignano@reply.it>
2025-12-02 15:47:57 +0100

    Added initial setup for the project

---
+++ src/main/java/com/carignano/nasa_exercise/NasaExerciseApplication.java
@@ -0,0 +1,13 @@
+package com.carignano.nasa_exercise;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class NasaExerciseApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(NasaExerciseApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/carignano/nasa_exercise/NasaExerciseApplicationTests.java
@@ -0,0 +1,13 @@
+package com.carignano.nasa_exercise;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class NasaExerciseApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
